0e443a812c3b5b3a6b418d3f31839761ed696830
iamvirgoo <argentum7732004@gmail.com>
2024-01-15 21:47:44 +0300

    FIX: sw caches

--- app/public/service-worker.js
+++ app/public/service-worker.js
@@ -1,53 +1,45 @@
-// public/service-worker.js
-
 const CACHE_NAME = 'sensus-cache-data';
 
-const urlsToCache = [
-	"/",
-	"/index.html",
-	"/src/global.scss",
-	"/src/styles/**/*.scss", // Шаблон для всех стилей в папке styles
-	"/src/assets/**/*.svg",   // Шаблон для всех SVG в папке assets
-];
-
 self.addEventListener("install", (event) => {
-	event.waitUntil(
-		caches.open(CACHE_NAME).then((cache) => {
-		return cache.addAll(urlsToCache);
-		})
-	);
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                return cache.addAll([
+					"/",
+					"../index.html",
+					"../src/global.scss",
+					"../src/styles/**/*.scss",
+					"../src/assets/**/*.svg",
+                ]);
+            })
+    );
 });
 
+// Активация Service Worker
 self.addEventListener("activate", (event) => {
-	event.waitUntil(
-		caches.keys().then((cacheNames) => {
-		return Promise.all(
-			cacheNames.map((cacheName) => {
-			if (cacheName !== CACHE_NAME) {
-				return caches.delete(cacheName);
-			}
-			})
-		);
-		})
-	);
+    event.waitUntil(
+        caches.keys()
+            .then((cacheNames) => {
+                return Promise.all(
+                    cacheNames.map((cacheName) => {
+                        if (cacheName !== CACHE_NAME) {
+                            return caches.delete(cacheName);
+                        }
+                    })
+                );
+            })
+    );
 });
 
+// Получение запросов и возврат кэшированных данных, если такие есть, иначе выполнение сетевого запроса
 self.addEventListener("fetch", (event) => {
-	event.respondWith(
-		caches.match(event.request).then((cachedResponse) => {
-		const fetchPromise = fetch(event.request)
-			.then((networkResponse) => {
-			if (networkResponse.ok) {
-				const clone = networkResponse.clone();
-				caches.open(CACHE_NAME).then((cache) => {
-				cache.put(event.request, clone);
-				});
-			}
-			return networkResponse;
-			})
-			.catch(() => cachedResponse);
-
-		return cachedResponse || fetchPromise;
-		})
-	);
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
 });
