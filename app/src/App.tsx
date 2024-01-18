f6f795d4e80b9aa10a9b664f5a8f7a46c49346ed
iamvirgoo <argentum7732004@gmail.com>
2024-01-18 15:16:42 +0300

    FIX: update import

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import './global.scss'
-import './styles/index.scss'
-import './styles/circle.scss'
-import './styles/metric.scss'
-import './styles/authForm.scss'
-import './styles/modal.scss'
+import './global.scss';
+import './styles/index.scss';
+import './styles/circle.scss';
+import './styles/metric.scss';
+import './styles/authForm.scss';
+import './styles/modal.scss';
+import './styles/wind.scss';
 import 'swiper/css';
 
 import { BrowserRouter, Route, Routes } from "react-router-dom";
@@ -14,7 +15,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import IndexPage from "./pages/indexPage";
 import DetailsPage from "./pages/detailsPage";
 
-navigator.serviceWorker.register('service-worker.js');
+// navigator.serviceWorker.register('service-worker.js');
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
